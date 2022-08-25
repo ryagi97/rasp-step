767dbb39177711dc880db9a0dffc324affaddca6
ryagi <ryagius17@gmail.com>
2022-08-25 21:48:54 +0200

    added gpiod test
    Signed-off-by: ryagi <ryagius17@gmail.com>

---
+++ tests/gpiod/gpiod.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <gpiod.h>
+#include <gpiod.hpp>
+
+using namespace std::literals::chrono_literals;
+
+const int pin = 17;
+
+int main(void)
+{
+    gpiod::chip chip ("gpiodchip0");
+    auto line = chip.get_line(pin);
+    line.request({"test",
+                  gpiod::line_request::DIRECTION_OUTPUT,
+                  0}, 0);
+
+    while (true) {
+        line.set_value(1);
+        std::cout << "ON" << std::endl;
+        std::this_thread::sleep_for(100ms);
+        line.set_value(0);
+        std::cout << "OFF" << std::endl;
+        std::this_thread::sleep_for(100ms);
+    }
+    return 0;
+}
